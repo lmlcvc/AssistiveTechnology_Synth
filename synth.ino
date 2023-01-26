76a0c47c403cd4893530a7e9e56cc0959abded4f
Lana Miličević <lana.milicevic10@gmail.com>
2023-01-26 17:47:10 +0100

    Added buttons state reading and MIDI basic functionality

--- synth.ino
+++ synth.ino
@@ -1,24 +1,40 @@
-#define LED_INTERVAL_MS       1     // TODO might need adjustment when 12 LEDS on
+#define INTERVAL_MS_LED       1     // TODO might need adjustment when 12 LEDS on
+#define INTERVAL_MS_BUTTONS   200
 #define SERIAL_RATE           9600
 
-// TODO: rewrite when all LEDs added
-#define LED_SELECT_PIN_6  6
-#define LED_SELECT_PIN_7  7
-#define LED_SELECT_PIN_8  8
+// TODO: rewrite when all added
+#define BUTTON_1_PIN          2
+#define BUTTON_CHORD_PIN      4
+#define LED_SELECT_PIN_6      6
+#define LED_SELECT_PIN_7      7
+#define LED_SELECT_PIN_8      8
 
-#define LED_PIN_R         9
-#define LED_PIN_G         10
-#define LED_PIN_B         11
+#define LED_PIN_R             9
+#define LED_PIN_G             10
+#define LED_PIN_B             11
+
+#define MIDI_CMD_NOTE_OFF    0x80
+#define MIDI_CMD_NOTE_ON     0x90
+#define MIDI_CMD_OCTAVE_DOWN 0xF4
+#define MIDI_CMD_OCTAVE_UP   0xF5
+#define MIDI_CMD_CHORDS      0xF9
+
+#define MIDI_BASE_TONE       0x0C    // C1
+#define MIDI_VELOCITY        0x45
+#define TONE_COUNT           12
 
 
 unsigned long currentTime;
 unsigned long previousTimeLeds;
+unsigned long previousTimeButtons;
 
+const uint8_t crtlButtons[] = {BUTTON_CHORD_PIN};
+const uint8_t crtlCommands[] = {MIDI_CMD_OCTAVE_DOWN, MIDI_CMD_OCTAVE_UP, MIDI_CMD_CHORDS};
+const uint8_t toneButtons[] = {BUTTON_1_PIN};
 const uint8_t outLeds[] = {LED_SELECT_PIN_8, LED_SELECT_PIN_7, LED_SELECT_PIN_6};
 const uint8_t colourLeds[] = {LED_PIN_R, LED_PIN_G, LED_PIN_B};
 
 const int outLedCount = sizeof(outLeds) / sizeof(outLeds[0]);
-unsigned int outLedPrevs[12] = {0};
 uint8_t led = -1;
 
 const int colours[12][3] = {{255, 0, 0},      // RED
@@ -38,6 +54,14 @@ const int colours[12][3] = {{255, 0, 0},      // RED
 void setup() {
   Serial.begin(SERIAL_RATE);
 
+  for(int i = 0; i < sizeof(crtlButtons); i++) {
+    pinMode(crtlButtons[i], INPUT);
+  }
+
+  for(int i = 0; i < sizeof(toneButtons); i++) {
+    pinMode(toneButtons[i], INPUT);
+  }
+
   for(int i = 0; i < sizeof(colourLeds); i++) {
     pinMode(colourLeds[i], OUTPUT);
   }
@@ -47,7 +71,45 @@ void setup() {
     digitalWrite(outLeds[i], LOW);
   }
 
-  previousTimeLeds = millis();
+  previousTimeLeds, previousTimeButtons = millis();
+}
+
+
+// send midi control command
+uint8_t sendCtrl(int cmd) {
+  if(cmd < 127) return 1;
+
+  Serial.write(cmd);
+  return 0;
+}
+
+// send note via midi as command, pitch, velocity
+uint8_t sendNote(int cmd, int pitch, int velocity) {
+  if(cmd < 127) return 1;
+  if(pitch >= 127) return 2;
+
+  Serial.write(cmd);
+  Serial.write(pitch);
+  Serial.write(velocity);
+  return 0;
+}
+
+
+int8_t readButtons() {  // TODO handle returned value errors (ovo se neće dešavat, ali svejedno)
+  for(int i = 0; i < sizeof(toneButtons); i++) {
+    if (digitalRead(BUTTON_CHORD_PIN) == HIGH) {  
+      sendCtrl(crtlCommands[i]);
+    } 
+  }
+
+  
+  for(int i = 0; i < sizeof(toneButtons); i++) {
+    if (digitalRead(toneButtons[i]) == HIGH) {
+      sendNote(MIDI_CMD_NOTE_ON, MIDI_BASE_TONE + i, MIDI_VELOCITY);
+    } 
+  }
+
+  return 0;   // no error
 }
 
 
@@ -55,8 +117,7 @@ void loop() {
   currentTime = millis(); 
 
   /* -------------------- LED colours -------------------- */
-  Serial.println(currentTime - previousTimeLeds);
-  if ((currentTime - previousTimeLeds) > LED_INTERVAL_MS) {  
+  if ((currentTime - previousTimeLeds) > INTERVAL_MS_LED) {  
     led = (led + 1) % outLedCount;
     uint8_t prev = (led - 1 >= 0) ? led-1 : outLedCount-1;
     
@@ -69,5 +130,10 @@ void loop() {
     digitalWrite(outLeds[led], HIGH);
     
     previousTimeLeds = millis();
+  }
+
+  if ((currentTime - previousTimeButtons) > INTERVAL_MS_BUTTONS) {  
+    readButtons();
+    previousTimeButtons = millis();
   }
 }
